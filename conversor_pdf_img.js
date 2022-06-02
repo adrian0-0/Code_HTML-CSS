f49bfe7c576d2d3e3ed3aeba5178230436331b7d
Maximize <maximize@Mac-mini-de-Maximize.local>
2022-06-02 18:39:12 -0300

    wip

---
+++ conversor_pdf_img.js
@@ -0,0 +1,16 @@
+//https://github.com/ol-th/pdf-img-convert.js
+var pdf2img = require('pdf-img-convert');
+
+var outputImages1 = pdf2img.convert('/Users/maximize/Documents/Adrian/VS_CODE/conversor_pdf_html/assets/project_proposal.pdf');
+var outputImages2 = pdf2img.convert('/Users/maximize/Documents/Adrian/VS_CODE/conversor_pdf_html/assets/meeting_notes.pdf');
+
+// From here, the images can be used for other stuff or just saved if that's required:
+
+var fs = require('fs');
+
+outputImages1.then(function(outputImages) {
+    for (i = 0; i < outputImages.length; i++)
+        fs.writeFile("output"+i+".png", outputImages[i], function (error) {
+          if (error) { console.error("Error: " + error); }
+        });
+    });
